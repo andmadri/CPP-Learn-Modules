118dd86796f819b4fd64083bafcc6d062f4f0b64
Andres Madrid Ucha <andmadri@f0r3s25.codam.nl>
2025-01-16 19:31:33 +0100

    working on ex02

---
+++ C09/ex02/src/PmergeMe.cpp
@@ -0,0 +1,6 @@
+
+void sortingAlgorithm(std::vector<int> elements) {
+	if (elements.size() <= 1) {
+		//then do not sort it
+	}
+}
---
+++ C09/ex02/src/main.cpp
@@ -0,0 +1,16 @@
+#include "../incl/PmergeMe.hpp"
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		std::cerr << "Error: not correct number of arguments" << std::endl;
+	}
+	//able to use a positive integer sequence as argument (Ford-Johnson algorithm)
+	//use the merge-insert sort algorithm to sort the positive integer squence
+	//error message in the standard error
+	//two different containers. your program must be able to handle at least 3000 different integers
+}
+
+//first unsorted positive integer sequence
+//then sorted sequence
+//time elapsed with a container in us
+//time elapsed with the other container in us
